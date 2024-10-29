33e99e30250b82327aa0dad2b8ef38073695fc94
Norbert van Nobelen <norbert.van.nobelen2@gmail.com>
2024-10-29 11:08:06 -0700

    ehn: Review and refactor for process state

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ rd=$(git rev-parse --show-toplevel)
 cd $rd
 
 protoc \
---proto_path=. "enums.proto" \
+--proto_path=. "util.proto" \
 --proto_path=$(dirname $(dirname "$rd")) \
 "--go_out=." --go_opt=paths=source_relative 
 
@@ -34,6 +34,13 @@ protoc \
 
 protoc \
 --proto_path=. "alpaca-grpc.proto" \
+--proto_path=$(dirname $(dirname "$rd")) \
+"--go_out=." --go_opt=paths=source_relative \
+--go-grpc_opt=require_unimplemented_servers=false \
+"--go-grpc_out=." --go-grpc_opt=paths=source_relative
+
+protoc \
+--proto_path=. "broker-smartcontract-grpc.proto" \
 --proto_path=$(dirname $(dirname "$rd")) \
 "--go_out=." --go_opt=paths=source_relative \
 --go-grpc_opt=require_unimplemented_servers=false \
