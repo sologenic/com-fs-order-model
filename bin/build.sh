129254eda07e35ebc1ef5a328549d0116cabb9c5
“jayjang0209” <jayjang0209@gmail.com>
2025-05-26 14:23:09 -0700

    ehn: generate grpc for typescript

--- bin/build.sh
+++ bin/build.sh
@@ -52,3 +52,77 @@ protoc \
 "--go_out=." --go_opt=paths=source_relative \
 --go-grpc_opt=require_unimplemented_servers=false \
 "--go-grpc_out=." --go-grpc_opt=paths=source_relative
+
+# if there's TS project in the package, generate a protobuf file for TS
+rm -rf node_modules
+npm i
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    util.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    broker.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    order.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    attestation.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    order-grpc.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    broker-grpc.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    smartcontractlog-grpc.proto
+
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+    --proto_path=. \
+    --proto_path=$(dirname $(dirname "$rd")) \
+    --ts_proto_out=. \
+    --ts_proto_opt=esModuleInterop=true \
+    --ts_proto_opt=outputServices=grpc-js \
+    attestation-grpc.proto
+
+npm run build
+git add build/
+
+git add *.ts
+rm -rf node_modules
