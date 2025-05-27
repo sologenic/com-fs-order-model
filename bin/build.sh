b60c13efd72c7e170df367a6a00e44a4e7a06967
“jayjang0209” <jayjang0209@gmail.com>
2025-05-27 10:54:52 -0700

    fix: rebuild to fix undefined issue leading to crash react app

--- bin/build.sh
+++ bin/build.sh
@@ -62,7 +62,6 @@ protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --proto_path=$(dirname $(dirname "$rd")) \
     --ts_proto_out=. \
     --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
     util.proto
 
 protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
@@ -70,7 +69,6 @@ protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --proto_path=$(dirname $(dirname "$rd")) \
     --ts_proto_out=. \
     --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
     broker.proto
 
 protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
@@ -78,7 +76,6 @@ protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --proto_path=$(dirname $(dirname "$rd")) \
     --ts_proto_out=. \
     --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
     order.proto
 
 protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
@@ -86,40 +83,7 @@ protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --proto_path=$(dirname $(dirname "$rd")) \
     --ts_proto_out=. \
     --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
     attestation.proto
-
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-    --proto_path=. \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    --ts_proto_out=. \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
-    order-grpc.proto
-
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-    --proto_path=. \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    --ts_proto_out=. \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
-    broker-grpc.proto
-
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-    --proto_path=. \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    --ts_proto_out=. \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
-    smartcontractlog-grpc.proto
-
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-    --proto_path=. \
-    --proto_path=$(dirname $(dirname "$rd")) \
-    --ts_proto_out=. \
-    --ts_proto_opt=esModuleInterop=true \
-    --ts_proto_opt=outputServices=grpc-js \
-    attestation-grpc.proto
 
 npm run build
 git add build/
--- build/index.js
+++ build/index.js
@@ -2,7 +2,4 @@ export * from "./order";
 export * from "./broker";
 export * from "./attestation";
 export * from "./util";
-export * from "./order-grpc";
-export * from "./attestation-grpc";
-export * from "./smartcontractlog-grpc";
 export { protobufPackage } from "./order";
